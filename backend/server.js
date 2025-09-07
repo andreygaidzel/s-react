30dd31bad81bda66a96f47e528e65fbe4a3bc429
Andrey <andrey.gaidzel@gmail.com>
2025-09-07 13:30:08 +0300

    add server endpoints for products

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ import cors from 'cors';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
 
+import productRoutes from './routes/productRoutes.js';
+import { sql } from './config/db.js';
+
 dotenv.config();
 
 const app = express();
@@ -14,6 +17,28 @@ app.use(cors());
 app.use(helmet()); // HTTP headers security
 app.use(morgan("dev")); // log the requests
 
-app.listen(PORT, () => {
-  console.log('Server started on port ' + PORT);
+app.use("/api/products", productRoutes);
+
+async function initDB(){
+  try {
+    await sql`
+      CREATE TABLE IF NOT EXISTS products (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        image VARCHAR(255) NOT NULL,
+        price DECIMAL(10, 2) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
+    `;
+
+    console.log("Database initialized");
+  } catch (error) {
+    console.log("Error init DB", error);
+  }
+}
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log('Server started on port ' + PORT);
+  });
 });
