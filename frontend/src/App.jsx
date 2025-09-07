9e57bd1619f54ba2d446389b910570132c3a0714
Andrey <andrey.gaidzel@gmail.com>
2025-09-07 16:44:59 +0300

    init frontend app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import Navbar from './components/Navbar.jsx';
+
+import HomePage from './pages/HomePage.jsx';
+import ProductPage from './pages/ProductPage.jsx';
+
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-base-200 transition-color duration-300">
+
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App
---
+++ frontend/src/pages/ProductPage.jsx
@@ -0,0 +1,4 @@
+function ProductPage() {
+  return <div>Product</div>
+}
+export default ProductPage;
