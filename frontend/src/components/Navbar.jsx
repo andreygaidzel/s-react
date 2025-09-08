be4898f69bc94cb49c2ada03c593ad18124c0bb6
Andrey <andrey.gaidzel@gmail.com>
2025-09-08 21:08:08 +0300

    fix products counter

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,13 +2,13 @@ import { ShoppingBagIcon, ShoppingCartIcon } from 'lucide-react';
 import React from 'react';
 import { Link, useResolvedPath } from 'react-router-dom';
 import ThemeSelector from './ThemeSelector.jsx';
-import { useThemeStore } from '../store/useThemeStore.js';
+import { useProductStore } from '../store/useProductStore.js';
 
 function Navbar() {
   const { pathname } = useResolvedPath();
   const isHomePage = pathname === "/";
 
-  const { theme, setTheme } = useThemeStore();
+  const { products } = useProductStore();
 
   return <div className="bg-base-100/80 backdrop-blur-lg border-b border-b-base-conten/10 sticky top-0 z-50">
     <div className="max-w-7xl mx-auto">
@@ -32,7 +32,9 @@ function Navbar() {
             <div className="indicator">
               <div className="p-2 rounded-full hover:bg-base-200 transition-color">
                 <ShoppingBagIcon className="size-5" />
-                <span className="badge badge-sm badge-primary indicator-item">8</span>
+                <span className="badge badge-sm badge-primary indicator-item">
+                  {products.length}
+                </span>
               </div>
             </div>
           )}
